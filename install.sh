f30f2a857e0bd61635d00beee35e4191e8bec206
sfuruya <shoichi.furuya.0612@gmail.com>
2019-07-04 12:31:36 +0900

    gcloud,terraform,sam,kubectlを追加

--- install.sh
+++ install.sh
@@ -2,26 +2,26 @@
 
 echo "Start dotfiles install.\n"
 
-echo "Homebrew existence check."
 type brew
 if [ $? != 0 ]; then
     echo "Install Homebrew.\n"
     /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 fi
+echo ""
 
-echo "Ansible existence check."
 type ansible
 if [ $? != 0 ]; then
     echo "Install ansible.\n"
     /usr/local/bin/brew install ansible
 fi
+echo ""
 
-echo "Python3 existence check."
 type python3
 if [ $? != 0 ]; then
     echo "Install python3.\n"
     /usr/local/bin/brew install python3
 fi
+echo ""
 
 echo "Execute playbook.\n"
 /usr/local/bin/ansible-playbook main.yml -i inventories/inventory
