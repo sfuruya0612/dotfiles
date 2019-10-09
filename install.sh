22d9be3af196ab7279d851a62e2193e46b372c91
sfuruya <shoichi.furuya.0612@gmail.com>
2019-10-09 14:05:43 +0900

    出力をみやすくした

--- install.sh
+++ install.sh
@@ -1,31 +1,34 @@
 # /bin/sh
 
-echo "Start dotfiles install.\n"
+echo "\033[33mStart dotfiles install.\033[00m\n"
 
-type brew
+type brew > /dev/null
 if [ $? != 0 ]; then
-    echo "Install Homebrew.\n"
+    echo "Install Homebrew."
     /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+else
+    echo "\033[32mHomebrew is already installed.\033[00m"
 fi
-echo ""
 
-type ansible
+type ansible > /dev/null
 if [ $? != 0 ]; then
-    echo "Install ansible.\n"
+    echo "Install ansible."
     /usr/local/bin/brew install ansible
+else
+    echo "\033[32mAnsible is already installed.\033[00m"
 fi
-echo ""
 
-type python3
+type python3 > /dev/null
 if [ $? != 0 ]; then
-    echo "Install python3.\n"
+    echo "Install python3."
     /usr/local/bin/brew install python3
+else
+    echo "\033[32mPython3 is already installed.\033[00m\n"
 fi
-echo ""
 
-echo "Execute playbook.\n"
+echo "Execute Ansible playbook.\nEnter sudo password."
 /usr/local/bin/ansible-playbook main.yml \
     -i inventory/local \
     --ask-become-pass
 
-echo "\nInstall finished.\n"
+echo "\033[33mInstall finished.\033[00m\n"
