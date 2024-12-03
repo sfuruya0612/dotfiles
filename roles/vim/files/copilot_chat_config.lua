ea0f5c6a86d0c0125838a138638186fa1d788efa
sfuruya0612 <shoichi.furuya.0612@gmail.com>
2024-12-03 23:34:17 +0900

    初期構築時のエラーを解消

--- roles/vim/files/copilot_chat_config.lua
+++ roles/vim/files/copilot_chat_config.lua
@@ -4,10 +4,10 @@ local actions = require("CopilotChat.actions")
 
 
 require("CopilotChat").setup {
-  debug = true,        -- Enable debugging
+  debug = true,      -- Enable debugging
 
-  model = 'gpt-4',     -- GPT model to use, 'gpt-3.5-turbo' or 'gpt-4'
-  temperature = 0.1,   -- GPT temperature
+  model = 'gpt-4',   -- GPT model to use, 'gpt-3.5-turbo' or 'gpt-4'
+  temperature = 0.1, -- GPT temperature
 
   prompts = {
     Explain = {
@@ -33,17 +33,17 @@ require("CopilotChat").setup {
 
   -- default window options
   window = {
-    layout = 'vertical',        -- 'vertical', 'horizontal', 'float', 'replace'
-    width = 0.3,                -- fractional width of parent, or absolute width in columns when > 1
-    height = 0.3,               -- fractional height of parent, or absolute height in rows when > 1
+    layout = 'vertical',    -- 'vertical', 'horizontal', 'float', 'replace'
+    width = 0.3,            -- fractional width of parent, or absolute width in columns when > 1
+    height = 0.3,           -- fractional height of parent, or absolute height in rows when > 1
     -- Options below only apply to floating windows
-    relative = 'editor',        -- 'editor', 'win', 'cursor', 'mouse'
-    border = 'single',          -- 'none', single', 'double', 'rounded', 'solid', 'shadow'
-    row = nil,                  -- row position of the window, default is centered
-    col = nil,                  -- column position of the window, default is centered
-    title = 'Copilot Chat',     -- title of chat window
-    footer = nil,               -- footer of chat window
-    zindex = 1,                 -- determines if window is on top or below other floating windows
+    relative = 'editor',    -- 'editor', 'win', 'cursor', 'mouse'
+    border = 'single',      -- 'none', single', 'double', 'rounded', 'solid', 'shadow'
+    row = nil,              -- row position of the window, default is centered
+    col = nil,              -- column position of the window, default is centered
+    title = 'Copilot Chat', -- title of chat window
+    footer = nil,           -- footer of chat window
+    zindex = 1,             -- determines if window is on top or below other floating windows
   },
 
   -- default mappings
@@ -74,12 +74,12 @@ require("CopilotChat").setup {
     show_diff = {
       normal = 'gd'
     },
-    show_system_prompt = {
-      normal = 'gp'
-    },
-    show_user_selection = {
-      normal = 'gs'
-    },
+    -- show_system_prompt = {
+    --   normal = 'gp'
+    -- },
+    -- show_user_selection = {
+    --   normal = 'gs'
+    -- },
   },
 
 }
